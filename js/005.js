6130bf8fe825f39884710aca31ad70d35e1cdb8f
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-05-14 12:57:58 -0400

    Add solution for Meeting 6kyu kata

---
+++ js/005.js
@@ -0,0 +1,29 @@
+function meeting(s) {
+  return `(${s
+    .toUpperCase()
+    .split(";")
+    .map((ele) => ele.split(":").reverse().join(", "))
+    .sort()
+    .join(")(")})`;
+}
+
+console.log(
+  meeting(
+    "Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn"
+  )
+);
+// "(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)"
+
+console.log(
+  meeting(
+    "John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell"
+  )
+);
+//  "(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)"
+
+console.log(
+  meeting(
+    "Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern"
+  )
+);
+// "(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)"
